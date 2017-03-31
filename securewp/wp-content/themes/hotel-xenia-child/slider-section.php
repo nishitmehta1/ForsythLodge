486b425f2bd1266d47ebffaa1d4f8617d7779617
Jignesh Chudasama <jigneshchudasama91@gmail.com>
2017-03-31 12:46:49 +0530

    started with slider section

--- securewp/wp-content/themes/hotel-xenia-child/slider-section.php
+++ securewp/wp-content/themes/hotel-xenia-child/slider-section.php
@@ -1,25 +1,27 @@
 <div class="slider-section">
 	<div class="row">
-		<div class="col-md-6">
-			<img src="<?php echo get_bloginfo("template_url"); ?>/images/slide-1.jpg"/>
+		<div class="col-md-6 slider-left">
+			<div id="sync1" class="owl-carousel owl-theme">
+			  <div class="item"><img src="<?php echo get_bloginfo("template_url"); ?>/images/slide-1.jpg"/></div>
+			  <div class="item"><img src="<?php echo get_bloginfo("template_url"); ?>/images/slide-2.jpg"/></div>
+			</div>
 		</div>
 		<div class="col-md-6">
 			<div class="col-md-6">
-				<div class="owl-carousel owl-theme">
-				    <div class="item"><h4>1</h4></div>
-				    <div class="item"><h4>2</h4></div>
-				    <div class="item"><h4>3</h4></div>
-				    <div class="item"><h4>4</h4></div>
-				    <div class="item"><h4>5</h4></div>
-				    <div class="item"><h4>6</h4></div>
-				    <div class="item"><h4>7</h4></div>
-				    <div class="item"><h4>8</h4></div>
-				    <div class="item"><h4>9</h4></div>
-				    <div class="item"><h4>10</h4></div>
-				    <div class="item"><h4>11</h4></div>
-				    <div class="item"><h4>12</h4></div>
+				<div id="sync2" class="owl-carousel owl-theme">
+				  	<div class="item">
+				  		<p>EXPERIENCES</p>
+				    	<h2>Satpura Walk Safaris </h2>
+				    	<span><img src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/></span>
+				    	<p class="description">When you’ve got past the preliminaries; there is landscape that opens out like an ancient text to reveal the hieroglyphs of animal presence, there are trees to touch, birds to follow, and an endless series of insect curiosities to stop and wonder at.</p>
+				    </div>
+				    <div class="item">
+				  		<p>EXPERIENCES 1</p>
+				    	<h2>Satpura Walk Safaris 1</h2>
+				    	<span><img src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/></span>
+				    	<p class="description">When you’ve got past the preliminaries; there is landscape that opens out like an ancient text to reveal the hieroglyphs of animal presence, there are trees to touch, birds to follow, and an endless series of insect curiosities to stop and wonder at.</p>
+				    </div>
 				</div>
-				Text
 			</div>
 		</div>
 	</div>
--- securewp/wp-content/themes/hotel-xenia/assets/js/main.js
+++ securewp/wp-content/themes/hotel-xenia/assets/js/main.js
@@ -1,38 +1,91 @@
-
 (function() {
-    jQuery(function($) {
+  jQuery(function($) {
 
-        FLApp = {
+    FLApp = {
 
-            init: function() {
-                this.loadBindings();
-            },
+      init: function() {
+          this.loadBindings();
+      },
 
-            loadBindings: function() {
-                var thisApp = this;
+      loadBindings: function() {
+        var thisApp = this;
 
-                console.log('hello');
+        var sync1 = $("#sync1"),
+            sync2 = $("#sync2"),
+            syncedSecondary = true;
 
-                $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:3
-          },
-          1000:{
-              items:5
-          }
-      }
-  });
+          sync1.owlCarousel({
+            items : 1,
+            slideSpeed : 1000,
+            nav: true,
+            autoplay: false,
+            dots: true,
+            loop: true,
+            touchDrag  : false,
+     mouseDrag  : false,
+            transitionStyle: "fade",
+          }).on('changed.owl.carousel', syncPosition);
+
+          sync2
+            .on('initialized.owl.carousel', function () {
+              sync2.find(".owl-item").eq(0).addClass("current");
+            })
+            .owlCarousel({
+            items : 1,
+            dots: true,
+            nav: true,
+            slideSpeed : 1000,
+            slideBy: 1,
+            transitionStyle: "fade",
+          }).on('changed.owl.carousel', syncPosition2);
+
+          function syncPosition(el) {
+            var count = el.item.count-1;
+            var current = Math.round(el.item.index - (el.item.count/2) - .5);
+            
+            if(current < 0) {
+              current = count;
             }
-        };
+            if(current > count) {
+              current = 0;
+            }
+            
+            //end block
 
-        FLApp.init();
+            sync2
+              .find(".owl-item")
+              .removeClass("current")
+              .eq(current)
+              .addClass("current");
+            var onscreen = sync2.find('.owl-item.active').length - 1;
+            var start = sync2.find('.owl-item.active').first().index();
+            var end = sync2.find('.owl-item.active').last().index();
+            
+            if (current > end) {
+              sync2.data('owl.carousel').to(current, 100, true);
+            }
+            if (current < start) {
+              sync2.data('owl.carousel').to(current - onscreen, 100, true);
+            }
+          }
+  
+        function syncPosition2(el) {
+          if(syncedSecondary) {
+            var number = el.item.index;
+            sync1.data('owl.carousel').to(number, 100, true);
+          }
+        }
+  
+        sync2.on("click", ".owl-item", function(e){
+          e.preventDefault();
+          var number = $(this).index();
+          sync1.data('owl.carousel').to(number, 300, true);
+        });
+          
+      }
+    };
 
-    });
+    FLApp.init();
+
+  });
 })();
