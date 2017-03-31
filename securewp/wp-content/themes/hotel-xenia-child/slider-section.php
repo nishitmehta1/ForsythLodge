38adafeb7f623f83d340087ca71354f60aece1db
Jignesh Chudasama <jigneshchudasama91@gmail.com>
2017-03-31 14:22:34 +0530

    second slider section added

--- securewp/wp-content/themes/hotel-xenia-child/slider-section.php
+++ securewp/wp-content/themes/hotel-xenia-child/slider-section.php
@@ -35,3 +35,41 @@
 		</div>
 	</div>
 </div>
+
+
+<div class="slider-section">
+	<div class="row row-eq-height">
+		
+		<div class="col-md-6 content-carousel-parent">
+			<div class="col-md-12">
+				<div class="content-carousel">
+					<div id="sync22" class="owl-carousel owl-theme">
+					  	<div class="item">
+					  		<p class="tour-category">ACTIVITIES</p>
+					    	<h2 class="tour-title">Satpura Canoe Safaris</h2>
+					    	<span class="leaf-image"><img src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/></span>
+					    	<p class="tour-description">The magic of being in a canoe is to paddle quietly along the shore, drifting into little bays and inlets to watch birds and animals coming down to water unmindful of the boat - a wilderness experience to be savoured. And then to paddle back across water turned to liquid gold in the sunset while birds arrow back to roost across a purple sky.</p>
+					    </div>
+					    <div class="item">
+					  		<p class="tour-category">ACTIVITIES 1</p>
+					    	<h2 class="tour-title">Satpura Canoe Safaris 1</h2>
+					    	<span class="leaf-image"><img src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/></span>
+					    	<p class="tour-description">The magic of being in a canoe is to paddle quietly along the shore, drifting into little bays and inlets to watch birds and animals coming down to water unmindful of the boat - a wilderness experience to be savoured. And then to paddle back across water turned to liquid gold in the sunset while birds arrow back to roost across a purple sky.</p>
+					    </div>
+					</div>
+					<div class="tour-additional-info">
+						<div class="tour-buttons">
+							<a href="#" class="bordered-button">Explore all experiences</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 slider-left">
+			<div id="sync11" class="owl-carousel owl-theme">
+				<div class="item"><img src="<?php echo get_bloginfo("template_url"); ?>/images/slide-2.jpg"/></div>
+				<div class="item"><img src="<?php echo get_bloginfo("template_url"); ?>/images/slide-1.jpg"/></div>
+			</div>
+		</div>
+	</div>
+</div>
--- securewp/wp-content/themes/hotel-xenia/assets/js/main.js
+++ securewp/wp-content/themes/hotel-xenia/assets/js/main.js
@@ -5,83 +5,156 @@
 
       init: function() {
           this.loadBindings();
+          this.syncOwlCarouselFirstSection();
+          this.syncOwlCarouselSecondSection();
       },
 
-      loadBindings: function() {
-        var thisApp = this;
-
+      syncOwlCarouselFirstSection: function() {
         var sync1 = $("#sync1"),
-            sync2 = $("#sync2"),
-            syncedSecondary = true;
+          sync2 = $("#sync2"),
+          syncedSecondary = true;
 
-          sync1.owlCarousel({
-            items : 1,
-            slideSpeed : 1000,
-            nav: true,
-            autoplay: false,
-            dots: true,
-            loop: true,
-            touchDrag  : false,
-            mouseDrag  : false,
-            transitionStyle: "fade",
-          }).on('changed.owl.carousel', syncPosition);
+        sync1.owlCarousel({
+          items : 1,
+          slideSpeed : 1000,
+          nav: true,
+          autoplay: false,
+          dots: true,
+          loop: true,
+          touchDrag  : false,
+          mouseDrag  : false,
+          transitionStyle: "fade",
+        }).on('changed.owl.carousel', syncPosition);
+
+        sync2
+          .on('initialized.owl.carousel', function () {
+            sync2.find(".owl-item").eq(0).addClass("current");
+          })
+          .owlCarousel({
+          items : 1,
+          dots: true,
+          nav: true,
+          slideSpeed : 1000,
+          slideBy: 1,
+          transitionStyle: "fade",
+        }).on('changed.owl.carousel', syncPosition2);
+
+        function syncPosition(el) {
+          var count = el.item.count-1;
+          var current = Math.round(el.item.index - (el.item.count/2) - .5);
+          
+          if(current < 0) {
+            current = count;
+          }
+          if(current > count) {
+            current = 0;
+          }
 
           sync2
-            .on('initialized.owl.carousel', function () {
-              sync2.find(".owl-item").eq(0).addClass("current");
-            })
-            .owlCarousel({
-            items : 1,
-            dots: true,
-            nav: true,
-            slideSpeed : 1000,
-            slideBy: 1,
-            transitionStyle: "fade",
-          }).on('changed.owl.carousel', syncPosition2);
-
-          function syncPosition(el) {
-            var count = el.item.count-1;
-            var current = Math.round(el.item.index - (el.item.count/2) - .5);
-            
-            if(current < 0) {
-              current = count;
-            }
-            if(current > count) {
-              current = 0;
-            }
-            
-            //end block
-
-            sync2
-              .find(".owl-item")
-              .removeClass("current")
-              .eq(current)
-              .addClass("current");
-            var onscreen = sync2.find('.owl-item.active').length - 1;
-            var start = sync2.find('.owl-item.active').first().index();
-            var end = sync2.find('.owl-item.active').last().index();
-            
-            if (current > end) {
-              sync2.data('owl.carousel').to(current, 100, true);
-            }
-            if (current < start) {
-              sync2.data('owl.carousel').to(current - onscreen, 100, true);
-            }
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+          var onscreen = sync2.find('.owl-item.active').length - 1;
+          var start = sync2.find('.owl-item.active').first().index();
+          var end = sync2.find('.owl-item.active').last().index();
+          
+          if (current > end) {
+            sync2.data('owl.carousel').to(current, 100, true);
           }
-  
+          if (current < start) {
+            sync2.data('owl.carousel').to(current - onscreen, 100, true);
+          }
+        }
+
         function syncPosition2(el) {
           if(syncedSecondary) {
             var number = el.item.index;
             sync1.data('owl.carousel').to(number, 100, true);
           }
         }
-  
+
         sync2.on("click", ".owl-item", function(e){
           e.preventDefault();
           var number = $(this).index();
           sync1.data('owl.carousel').to(number, 300, true);
         });
+      },
+
+      syncOwlCarouselSecondSection: function() {
+        var sync11 = $("#sync11"),
+          sync22 = $("#sync22"),
+          syncedSecondary = true;
+
+        sync11.owlCarousel({
+          items : 1,
+          slideSpeed : 1000,
+          nav: true,
+          autoplay: false,
+          dots: true,
+          loop: true,
+          touchDrag  : false,
+          mouseDrag  : false,
+          transitionStyle: "fade",
+        }).on('changed.owl.carousel', syncPosition);
+
+        sync22
+          .on('initialized.owl.carousel', function () {
+            sync22.find(".owl-item").eq(0).addClass("current");
+          })
+          .owlCarousel({
+          items : 1,
+          dots: true,
+          nav: true,
+          slideSpeed : 1000,
+          slideBy: 1,
+          transitionStyle: "fade",
+        }).on('changed.owl.carousel', syncPosition2);
+
+        function syncPosition(el) {
+          var count = el.item.count-1;
+          var current = Math.round(el.item.index - (el.item.count/2) - .5);
           
+          if(current < 0) {
+            current = count;
+          }
+          if(current > count) {
+            current = 0;
+          }
+
+          sync22
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+          var onscreen = sync22.find('.owl-item.active').length - 1;
+          var start = sync22.find('.owl-item.active').first().index();
+          var end = sync22.find('.owl-item.active').last().index();
+          
+          if (current > end) {
+            sync22.data('owl.carousel').to(current, 100, true);
+          }
+          if (current < start) {
+            sync22.data('owl.carousel').to(current - onscreen, 100, true);
+          }
+        }
+
+        function syncPosition2(el) {
+          if(syncedSecondary) {
+            var number = el.item.index;
+            sync11.data('owl.carousel').to(number, 100, true);
+          }
+        }
+
+        sync22.on("click", ".owl-item", function(e){
+          e.preventDefault();
+          var number = $(this).index();
+          sync11.data('owl.carousel').to(number, 300, true);
+        });
+      },
+
+      loadBindings: function() {
+        var thisApp = this;
       }
     };
 
