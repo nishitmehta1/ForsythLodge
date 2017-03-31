5ea45008af47e3babada2b296ec4150c98c5b07e
Jignesh Chudasama <jigneshchudasama91@gmail.com>
2017-03-31 13:52:08 +0530

    first slider section done

--- securewp/wp-content/themes/hotel-xenia-child/slider-section.php
+++ securewp/wp-content/themes/hotel-xenia-child/slider-section.php
@@ -1,13 +1,13 @@
 <div class="slider-section">
-	<div class="row">
+	<div class="row row-eq-height">
 		<div class="col-md-6 slider-left">
 			<div id="sync1" class="owl-carousel owl-theme">
 			  <div class="item"><img src="<?php echo get_bloginfo("template_url"); ?>/images/slide-1.jpg"/></div>
 			  <div class="item"><img src="<?php echo get_bloginfo("template_url"); ?>/images/slide-2.jpg"/></div>
 			</div>
 		</div>
-		<div class="col-md-6">
-			<div class="col-md-8">
+		<div class="col-md-6 content-carousel-parent">
+			<div class="col-md-12 col-lg-8">
 				<div class="content-carousel">
 					<div id="sync2" class="owl-carousel owl-theme">
 					  	<div class="item">
@@ -22,6 +22,13 @@
 					    	<span class="leaf-image"><img src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/></span>
 					    	<p class="tour-description">When you’ve got past the preliminaries; there is landscape that opens out like an ancient text to reveal the hieroglyphs of animal presence, there are trees to touch, birds to follow, and an endless series of insect curiosities to stop and wonder at.</p>
 					    </div>
+					</div>
+					<div class="tour-additional-info">
+						<p class="tour-price"><span>Rs. </span>22,500</p>
+						<div class="tour-buttons">
+							<a href="#" class="filled-button">Add to cart</a>
+							<a href="#" class="bordered-button">Explore all experiences</a>
+						</div>
 					</div>
 				</div>
 			</div>
