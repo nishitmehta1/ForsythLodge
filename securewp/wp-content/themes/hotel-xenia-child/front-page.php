cd7ab7bf97dd8a36a9c747660d88fcefd5579665
Prabhat <prabhat.panda@skarma.com>
2017-03-31 14:02:01 +0530

    update

--- securewp/wp-content/themes/hotel-xenia-child/front-page.php
+++ securewp/wp-content/themes/hotel-xenia-child/front-page.php
@@ -1,8 +1,26 @@
+<?php
+/*
+Template Name: homepage template
+*/
+?>
+
 <?php get_header(); ?>
 
+
+<div class="background-image-section">
+	<div class="background-description">
+		<div class="textbox">
+			<h1>
+				Satpura National Park<br>
+				Highlands of Central Asia
+			</h1>
+		</div>
+	</div>
+</div>
 
 <?php include 'slider-section.php'; ?>
 
 
 
 <?php get_footer(); ?>
+
