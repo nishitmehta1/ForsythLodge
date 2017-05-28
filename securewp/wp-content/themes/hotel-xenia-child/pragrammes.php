be9739d3ac487838e5cc8ebdfcbf2783e5b3d633
nishitmehta1 <nishit.mehta1@gmail.com>
2017-05-28 19:33:11 +0530

    V1

---
+++ securewp/wp-content/themes/hotel-xenia-child/pragrammes.php
@@ -0,0 +1,148 @@
+<?php 
+
+	/*Template Name: programmes template*/
+
+?>
+
+ <?php get_header(); ?>
+
+ <div class="background-image-section inner-pages-bg">
+ 	<div class="background-description container">
+ 		<div class="text-center textbox">
+ 			<h1> Programmes </h1>
+ 		</div>
+ 	</div>
+ 	<div class="enquiry-band">
+ 		<img class="enquiry-topi" src="<?php echo get_bloginfo("template_url"); ?>/images/enquiry-topi.svg"/>
+ 		<div class="enquiry-topi"></div>
+ 		<form class="booking-enquiry">
+ 			<div class="date-picker-container">
+ 				<input class="enquiry-date-picker" id="arrival-date" type="text" name="arrival-date" placeholder="Select arrival date">
+ 			</div>
+ 			<div class="date-picker-container">
+ 				<input class="enquiry-date-picker" id="departure-date" type="text" name="departure-date" placeholder="Select departure date">
+ 			</div>
+ 			<input class="enquiry-submit" type="submit" value="Check availability">
+ 		</form>
+ 	</div>
+ </div>
+
+
+ <div id="initiative" class="initiative_div col-md-12 text-center">
+ 	<span class="initiative_span"> INITIATIVE </span>
+ 	<span class="leaf-image offset-md-6">
+ 		<img class="leaf" src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/>
+ 	</span>
+ 	<span class="main-image col-md-12">
+ 		<img src="https://placehold.it/1536x660" alt="Forsyth Initiative">
+ 	</span>
+	<span class="background-cream col-md-12 text-center">
+		<span class="text-on-cream col-md-8 text-left" style="padding: 0;margin: 95px auto;margin-left: 250px">
+			Forsyth Lodge makes every effort to tread as lightly as possible on the ecosystem while making an exciting encounter with nature possible for our guests. We are mindful that everything that we do gives back to the community and takes care of the environment.
+		</span>
+
+	</span>
+ </div>
+
+ <div id="eco" class="eco_div col-md-12 text-center">
+ 	<span class="eco_span"> ECO-FRIENDLY INFRASTRUCTURE </span>
+ 	<span class="leaf-image offset-md-6">
+ 		<img class="leaf" src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/>
+ 	</span>
+ 	<span class="main-image col-md-12">
+ 		<img src="https://placehold.it/1536x660" alt="Forsyth Initiative">
+ 	</span>
+	<span class="background-cream col-md-12 text-center">
+		<span class="text-on-cream col-md-8 text-left" style="padding: 0;margin: 95px auto;margin-left: 250px">
+			These efforts are reflected in the care with which our buildings are constructed, how we manage and recycle water, and the manner in which we find better alternatives to existing technology — be it quieter engines or solar-powered motors—when it comes to everyday access to the Satpura Park.
+
+		</span>
+	</span>
+ </div>
+
+  <div id="reforest" class="reforest_div col-md-12 text-center">
+  	<span class="reforest_span"> REFORESTATION </span>
+  	<span class="leaf-image offset-md-6">
+  		<img class="leaf" src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/>
+  	</span>
+  	<span class="main-image col-md-12">
+  		<img src="https://placehold.it/1536x660" alt="Forsyth Initiative">
+  	</span>
+ 	<span class="background-cream col-md-12 text-center">
+ 		<span class="text-on-cream col-md-8 text-left" style="padding: 0;margin: 95px auto;margin-left: 250px">
+ 			Our concern is equally reflected in the responsible practices we employ to aid the return of the jungle in the buffer zone. Our property uses wild fencing by and large, rather than wire fencing, to ensure the unhindered movement of species across their preferred routes. Most of our 32 acres is undeveloped, allowing the natural ecosystem to flourish, and it not uncommon for us to walk past shy spotted deer or catch sight of a few wild boar!
+ 		</span>
+
+ 	</span>
+  </div>
+
+   <div id="wildlife" class="wildlife_div col-md-12 text-center">
+   	<span class="wildlife_span"> WILDLIFE FRIENDLY </span>
+   	<span class="leaf-image offset-md-6">
+   		<img class="leaf" src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/>
+   	</span>
+   	<span class="main-image col-md-12">
+   		<img src="https://placehold.it/1536x660" alt="Forsyth Initiative">
+   	</span>
+  	<span class="background-cream col-md-12 text-center">
+  		<span class="text-on-cream col-md-8 text-left" style="padding: 0;margin: 95px auto;margin-left: 250px">
+  			We have also dug waterholes across the property to ensure that these species have reliably safe access to water. Over several years, we’ve planted a careful distribution of indigenous trees, shrubs, and grasses in consultation with scientists and wildlife experts to ensure that small mammals have adequate shelter within the lodge area.
+  			opacity: 1.00;
+
+  		</span>
+
+  	</span>
+   </div>
+
+    <div id="benefit" class="benefit_div col-md-12 text-center">
+    	<span class="benefit_span"> BENEFITTING LOCAL COMMUNITIES </span>
+    	<span class="leaf-image offset-md-6">
+    		<img class="leaf" src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/>
+    	</span>
+    	<span class="main-image col-md-12">
+    		<img src="https://placehold.it/1536x660" alt="Forsyth Initiative">
+    	</span>
+   	<span class="background-cream col-md-12 text-center">
+   		<span class="text-on-cream col-md-8 text-left" style="padding: 0;margin: 95px auto;margin-left: 250px">
+   			<p>Forsyth works on the principle that efforts to preserve wildlife and valuable habitats can succeed only if the surrounding communities participate actively. A good part of our effort focuses on the giving back and drawing in that this requires. We are currently creating a system of rewards for locals who reliably report wildlife activity in the buffer zone. This is one form of viable enlistment. We’re also exploring the possibility of getting members of the local communities to run home-stays for guests on longer treks and hikes. Women from the nearby villages come in to cook for our guests once a week - an initiative begun to equip them with great catering skills that could be put to use in the future.</p>
+   			<p>We continue to seed other forms of local entrepreneurship by sourcing many of our needs through local contacts. This ranges from food and supplies to taxi services between Forsyth and Bhopal airport, and also for short hops. All the support staff at Forsyth are locals who’ve been carefully trained to work to international standards. Mindful of the fact that laws protecting habitats can take away livelihoods and pitchfork people into confrontation with policies, we have partnered with the forest department in training fishermen to work the boats and canoes, and in training villagers to become trackers and, in the future, naturalists with English-speaking skills.</p>
+   		</span>
+
+   	</span>
+    </div>
+
+     <div id="support" class="support_div col-md-12 text-center">
+     	<span class="support_span"> SUPPORTING THE GOVERNMENT SCHOOL </span>
+     	<span class="leaf-image offset-md-6">
+     		<img class="leaf" src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/>
+     	</span>
+     	<span class="main-image col-md-12">
+     		<img src="https://placehold.it/1536x660" alt="Forsyth Initiative">
+     	</span>
+    	<span class="background-cream col-md-12 text-center">
+    		<span class="text-on-cream col-md-8 text-left" style="padding: 0;margin: 95px auto;margin-left: 250px">
+    			We support the Government School in Sarangpur village in several ways. We provide support material that is hard to source. On hearing of staffing problems at the school, our naturalists came forward to pay for a substitute teacher—an arrangement that filled the gap and got a qualified localite employment. We have also begun a Nature Club in the school where our naturalists talk to the kids about local species and get them to share our enthusiasm for wildlife. We hope to augment this effort with the children through nature walks and little trips to the park.
+    		</span>
+
+    	</span>
+     </div>
+
+	 <div id="collaboration" class="collaboration_div col-md-12 text-center">
+	 	<span class="collaboration_span"> COLLABORATION </span>
+	 	<span class="leaf-image offset-md-6">
+	 		<img class="leaf" src="<?php echo get_bloginfo("template_url"); ?>/images/leaf.svg"/>
+	 	</span>
+	 	<span class="main-image col-md-12">
+	 		<img src="https://placehold.it/1536x660" alt="Forsyth Initiative">
+	 	</span>
+		<span class="background-cream col-md-12 text-center">
+			<span class="text-on-cream col-md-8 text-left" style="padding: 0;margin: 95px auto;margin-left: 250px">
+				Forsyth Lodge is a member of Travel Operators for Tigers India (TOFT) which is a campaign to create a strongly responsible ethic in the conduct of wildlife tourism so that the benefits of tourism are harnessed for conservation and the negatives are minimised. TOFT operates an eco-audit certification programme called the PUG audit and Forsyth's has been rated as OUTSTANDING - the highest rating.
+			</span>
+
+		</span>
+	 </div>
+
+
+
+ <?php get_footer(); ?>
